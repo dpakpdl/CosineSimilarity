8c5b19891bc464763f673279a4538ce3acdc21a6
Deepak Paudel <>
2019-04-17 18:16:31 +0545

    cosine similarity between sentences in summary and sentences in non-summary texts

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CosineSimilarity
 Cosine Similarity between summary and full text
+
+`python3 -m virtualenv similarity`
+
+`source similarity/bin/activate`
+
+`pip install -r requirements.txt`
+
+`python3 main.py`
---
+++ main.py
@@ -0,0 +1,175 @@
+import csv
+import os
+import re
+from collections import Counter
+
+import nltk
+import pandas as pd
+from scipy import spatial
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+ATTACHMENT_PATH = "Attachments"
+
+
+def read_csv_file(filepath, usecols):
+    dataset = pd.read_csv(filepath, usecols=usecols)
+    return dataset
+
+
+def get_matched_summaries():
+    summaries_path = os.path.join(ATTACHMENT_PATH, "Summaries.csv")
+
+    summaries = read_csv_file(summaries_path,
+                              usecols=["adsh", "full_summary", "full_summary_len", "expense_summary", "expense_len"])
+    total_summaries = summaries.adsh.tolist()
+
+    non_summaries_path = os.path.join(ATTACHMENT_PATH, "Non-Summaries")
+    non_summaries_file_list = os.listdir(non_summaries_path)
+    non_summaries_file_list = [os.path.splitext(os.path.basename(file_path))[0] for file_path in
+                               non_summaries_file_list]
+
+    mapping_path = os.path.join(ATTACHMENT_PATH, "Mapping.csv")
+    mapping = read_csv_file(mapping_path, usecols=["accession_xbrl", "accession_not_xbrl"])
+
+    new_mapping = mapping[(mapping['accession_not_xbrl'].isin(non_summaries_file_list)) & (
+        mapping['accession_xbrl'].isin(total_summaries))]
+
+    return new_mapping, mapping, summaries
+
+
+def get_lines_from_paragraph(paragraph):
+    words_output = list()
+    for line in paragraph.split(". "):
+        words_output.append(remove_articles_preposition(line.strip()))
+    return words_output
+
+
+def remove_articles_preposition(sentence):
+    tokens = nltk.word_tokenize(sentence)
+    # remove special characters
+    tokens = list(filter(lambda x: x, map(lambda x: re.sub(r'[^A-Za-z0-9]+', '', x), tokens)))
+
+    # remove articles
+    tokens = [token for token in tokens if token.lower() not in ['a', 'an', 'the']]
+    tagged = nltk.pos_tag(tokens)
+
+    # remove prepositions and stem the words
+    stemmer = nltk.stem.SnowballStemmer('english')
+
+    clean_word_list = [stemmer.stem(x) for (x, y) in tagged if y != 'IN']
+    return clean_word_list
+
+
+def read_file(filepath):
+    with open(filepath, "r", encoding="utf8", errors='ignore') as input_file:
+        content = input_file.read()
+
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    sentences = sent_detector.tokenize(content.strip())
+
+    # remove sentences not ending with full stop
+    sentences = [re.sub('\s+', ' ', sentence) for sentence in sentences if sentence[-1] is '.']
+    return sentences
+
+
+def calculate_word_vector_count(summary_word_vector, non_summary_wordvector):
+    total_score = list()
+    for line in summary_word_vector:
+        cosine = list()
+        for line2 in non_summary_wordvector:
+            combine_word_vector = list(set(line + line2))
+            summary_count_vector = get_word_count(line, combine_word_vector)
+            non_summary_count_vector = get_word_count(line2, combine_word_vector)
+
+            cosine.append(get_cosine_similarity(summary_count_vector, non_summary_count_vector))
+        total_score.append(max(cosine))
+    total = get_average_similarity(total_score)
+    return total
+
+
+def get_average_similarity(total):
+    return sum(total) / len(total)
+
+
+def get_cosine_similarity(summary_count_vector, non_summary_count_vector):
+    cosine_value = spatial.distance.cosine(summary_count_vector, non_summary_count_vector)
+    if cosine_value >= 0.90:
+        result = 1
+    else:
+        result = 0
+    return result
+
+
+def get_word_count(to_count_list, word_list):
+    counts = Counter()
+    counts.update(to_count_list)
+    for word in word_list:
+        if not counts.get('word', None):
+            counts.update({word: 0})
+    return get_ordered_vector(counts, word_list)
+
+
+def get_ordered_vector(counter, vector):
+    return [counter.get(key, 0) for key in vector]
+
+
+def add_rows_to_output_csv(output_rows):
+    summaries_path = os.path.join(ATTACHMENT_PATH, "Summaries.csv")
+    with open(summaries_path, 'r') as csvinput:
+        with open('Output/summaries_output.csv', 'w') as csvoutput:
+            writer = csv.writer(csvoutput, lineterminator='\n')
+            reader = csv.reader(csvinput)
+            all_rows = []
+            row = next(reader)
+            row.append('number_of_sentences_in_summary')
+            row.append('number_of_sentences_in_non_summary')
+            row.append('similarity')
+            all_rows.append(row)
+
+            for row in reader:
+                for output_row in output_rows:
+                    if row[0] == output_row.get("summary_indentifier"):
+                        row.append(output_row.get("number_of_sentences_in_summary"))
+                        row.append(output_row.get("number_of_sentences_in_non_summary"))
+                        row.append(output_row.get("similarity"))
+                        all_rows.append(row)
+
+            writer.writerows(all_rows)
+
+
+def main():
+    output_rows = list()
+    new_mapping, mapping, summaries = get_matched_summaries()
+    counter = 0
+    for index, row in new_mapping.iterrows():
+        counter += 1
+        summary_file_identity, non_summary_file_identity = row['accession_xbrl'], row['accession_not_xbrl']
+        full_summary = summaries.loc[summaries["adsh"] == summary_file_identity, 'full_summary'].iloc[0]
+
+        summary_word_vector = get_lines_from_paragraph(full_summary)
+
+        number_of_summary_lines = len(summary_word_vector)
+        non_summary_file_path = os.path.join(ATTACHMENT_PATH, "Non-Summaries", non_summary_file_identity + '.txt')
+
+        non_summary_lines = read_file(non_summary_file_path)
+        number_of_non_summary_lines = len(non_summary_lines)
+
+        non_summary_word_vector = list()
+        for non_summary_line in non_summary_lines:
+            non_summary_word_vector.append(remove_articles_preposition(non_summary_line))
+
+        similarity = calculate_word_vector_count(summary_word_vector, non_summary_word_vector)
+        output_rows.append(dict(summary_indentifier=summary_file_identity,
+                                similarity=similarity,
+                                number_of_sentences_in_summary=number_of_summary_lines,
+                                number_of_sentences_in_non_summary=number_of_non_summary_lines))
+        if counter >= 10:
+            break
+    print(output_rows)
+    add_rows_to_output_csv(output_rows)
+
+
+if __name__ == "__main__":
+    main()
